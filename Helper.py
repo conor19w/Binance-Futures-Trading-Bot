0dab90370c12aa25bdc6c0771d1acf3f82d5bbcd
conor19w <63513457+conor19w@users.noreply.github.com>
2021-09-21 14:01:02 +0100

    Create Helper.py

---
+++ Helper.py
@@ -0,0 +1,141 @@
+from binance.client import Client
+from datetime import timezone,datetime,date,timedelta
+client = Client(api_key='',api_secret='') ##Binance keys needed to get historical data/ Trade on an account
+def get_Klines(symbol,TIME_INTERVAL,time_period):
+    Date = []
+    Open = []
+    Close = []
+    High =[]
+    Low = []
+    Volume = []
+    High_1min = []
+    Low_1min = []
+    ##klines for candlestick patterns and TA
+    if TIME_INTERVAL==1:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_1MINUTE,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==3:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_3MINUTE,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==5:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_5MINUTE,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==15:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_15MINUTE,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==30:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_30MINUTE,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==60:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_1HOUR,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==120:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_2HOUR,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==240:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_4HOUR,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==360:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_6HOUR,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==480:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_8HOUR,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==720:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_12HOUR,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==1440:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_1DAY,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==4320:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_3DAY,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==10080:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_1WEEK,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+    elif TIME_INTERVAL==40320:
+        for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_1MONTH,start_str=time_period):
+            Date.append(datetime.utcfromtimestamp((round(kline[0] / 1000))) + timedelta(hours=1))
+            Open.append(float(kline[1]))
+            Close.append(float(kline[4]))
+            High.append(float(kline[2]))
+            Low.append(float(kline[3]))
+            Volume.append(float(kline[7]))
+
+    ##klines used for stoploss and takeprofit
+    for kline in client.get_historical_klines_generator(symbol, Client.KLINE_INTERVAL_1MINUTE,start_str=time_period):
+        # print(kline)
+        #Date.append(datetime.utcfromtimestamp((round(kline[0]/1000)))+timedelta(hours=1))
+        High_1min.append(float(kline[2]))
+        Low_1min.append(float(kline[3]))
+    return Date,Open,Close,High,Low,Volume,High_1min,Low_1min
