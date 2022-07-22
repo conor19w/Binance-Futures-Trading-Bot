ee97499829f81405c0046096ed1b8b2e79ffe103
Conor White <conor19w@gmail.com>
2022-07-22 21:29:27 +0100

    New Update Bot now has a GUI, just run app.py to use the bot :)

--- Config_File.py
+++ Config_File.py
@@ -3,19 +3,22 @@ API_KEY = ''
 API_SECRET = ''
 
 ################## settings, these are very strategy dependant ensure you have enough data for your chosen strategy ##################################
-order_Size = .02
+order_Size = .04
 leverage = 10
-start_string = '4 hour ago' ## Buffer of candle sticks be careful if you don't provide enough the bot will throw an error
-Interval = '1m' ##candle sticks you want to trade
-Max_Number_Of_Trades = 1  ## How many positions we can have open at once
-use_heikin_ashi = 0 ## Create heikin ashi candles that can be referenced in Bot_Class.Bot.make_decision()
-use_trailing_stop = 0 ##If on we will use our TP value as the Activation price for a trailing stop loss
-trailing_stop_callback = 0.1 ##trailing stop percent, this is .1% range is [.1% - 5%] .ie [0.1 - 5]
-##################################################################################
-###################### Coins Traded ##############################################
-##Trade All Coins if True, can also specify a list of coins to trade instead. Example: symbol = ['ETHUSDT','BTCUSDT'] & set Trade_All_Coins = False
-Trade_All_Coins = True
-symbol = ['GMTUSDT']  ## If Trade_All_Coins is False then we list the coins we want to trade here, otherwise the bot will automatically get all coins and trade them
+start_string = '2 days ago'  ## Buffer of candle sticks be careful if you don't provide enough the bot will throw an error
+Interval = '5m'  ##candle sticks you want to trade
+Max_Number_Of_Trades = 10  ## How many positions we can have open at once
+use_trailing_stop = 0  ##If on we will use our TP value as the Activation price for a trailing stop loss
+trailing_stop_callback = 0.1  ##trailing stop percent, this is .1% range is [.1% - 5%] .ie [0.1 - 5]
 
-## If you are getting a rate limit error on startup this will add a delay for downloading candlesticks to start
-RATE_LIMIT_WAIT = False ## It will not slow down the bot, it will only slow down the startup by about (4 x (number of coins you're trading)) seconds
+## New vars needed for the gui, running script from terminal will also need these now
+strategy = 'StochRSIMACD'
+TP_choice = '%'
+SL_choice = 'x (ATR)'
+SL_mult = 1
+TP_mult = 3
+
+##Trade All Coins if True, can also specify a list of coins to trade instead. Example: symbol = ['ETHUSDT','BTCUSDT'] & set Trade_All_Coins = False
+Trade_All_Coins = False
+symbol = ['ETHUSDT', 'BTCUSDT', 'ADAUSDT', 'BNBUSDT', 'XRPUSDT', 'PEOPLEUSDT', 'NEARUSDT', 'DOGEUSDT', '1000SHIBUSDT']  ## If Trade_All_Coins is False then we list the coins we want to trade here, otherwise the bot will automatically get all coins and trade them
+
