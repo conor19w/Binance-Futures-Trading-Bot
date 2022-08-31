54d17944cdb363dfca436c00ef6178e98e922f43
Conor <conor19w@gmail.com>
2022-08-31 23:02:02 +0100

    Fix indexes & error logger

--- Bot-v1-for-developers/Helper.py
+++ Bot-v1-for-developers/Helper.py
@@ -221,7 +221,7 @@ class Trade_Manager:
             print(f"{symbol}: Error in close_position(), Error: {e}")
         except Exception as e:
             print(f"{time}: {e}")
-            log_error(e)
+            log_error(str(e))
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
